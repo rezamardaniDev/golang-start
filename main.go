bdf027fe28706b9e10cff801489e571ca92956e4
reza <mardanireza58@gmail.com>
2025-04-30 10:13:34 +0330

    Initial commit: project structure created

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	lst1 := []int{1, 2, 3, 4, 5, 6, 7, 8, 10}
+
+	for index, item := range lst1 {
+		fmt.Println("index:", index, "item:", item)
+	}
+}
